a8c230aff2d98dd720f5d72d1858c3d2a6e2f7dd
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2018-03-20 14:33:13 +0200

    added single version file; fixed issue with Operations.Serilog deploying readme.txt as a content. v0.8.3 built

--- Operations.Web/Properties/AssemblyInfo.cs
+++ Operations.Web/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -7,31 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Operations.Web")]
 [assembly: AssemblyDescription("Classical ASP.NET/MVC integration for the Operations framework")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Vladimir Levchuk")]
 [assembly: AssemblyProduct("Operations.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7ad15fbc-ce4d-45d8-ba7a-6752922546dd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.2.0")]
-[assembly: AssemblyInformationalVersion("0.8.2")]
-[assembly: AssemblyFileVersion("0.8.2.0")]
